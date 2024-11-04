0a5ff9aeede5aa997ef7884a7c2dbbe4a722060f
Naufal-Arsyi <naufalmarsyi12@gmail.com>
2024-11-04 11:38:31 +0700

    Add files via upload

---
+++ TI_23215057_Latihan18.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan6;
+
+/**
+ *
+ * @author Arsyi
+* NAma  : Naufal Miftahul Arsyi
+ * Kelas : Teknik Informatika
+ * NIM   : 23215057
+ * Deskripsi : program ini berfungsi untuk menampilkan Loop kelipatan 3,5 hingga 35 
+ */
+
+public class TI_23215057_Latihan18 {
+    public static void main(String[] args) {
+        double angka = 3.5;
+
+        
+        while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
